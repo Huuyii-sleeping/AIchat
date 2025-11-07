015cc9a7d40c5b39051e977b8b36d4c7249aa8ef
huuyii <3134846106@qq.com>
2025-11-07 19:43:26 +0800

    feat：实现模态框的展示

--- renderer/hooks/useDialog.ts
+++ renderer/hooks/useDialog.ts
@@ -1,13 +1,25 @@
 export function useDialog() {
-  //   const isDarkMode = usePreferredDark();
+  const isDarkMode = usePreferredDark();
   const createDialog = (opts: CreateDialogProps) => {
+    const overlay = document.createElement("div");
+    const isModel = opts.isModal !== false;
+    overlay.classList.add("dialog-overlay");
+    watchEffect(
+      () =>
+        (overlay.style.backgroundColor = isDarkMode.value
+          ? "rgba(0, 0, 0, 0.6)"
+          : "rgba(255, 255, 255, 0.5)")
+    );
     return new Promise<string>((resolve) => {
       window.api.createDialog(opts).then((res) => {
         resolve(res);
-        // todo: model
+        if (!isModel) return;
+        document.body.removeChild(overlay);
+        overlay?.classList?.remove("show");
       });
-
-      // todo  model模态层
+      if (!isModel) return;
+      document.body.appendChild(overlay);
+      setTimeout(() => overlay.classList.add("show"), 10);
     });
   };
   return {
